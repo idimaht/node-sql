e96b72b8060366b1f698b84049365e4d8be95c6e
idimaht <idimaht.x@gmail.com>
2021-11-01 14:03:52 +0700

    Update mssql

--- api.js
+++ api.js
@@ -2,6 +2,13 @@ const dboperations = require('./dboperations.js');
 const db = require('./dboperations.js');
 var World = require('./world');
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+
+
 
 dboperations.getWorld().then(result => {
     console.log(result);
