f34a8813e573066653ae353e4303bdb515f19e13
idimaht <idimaht.x@gmail.com>
2021-11-01 16:22:41 +0700

    Update query

--- api.js
+++ api.js
@@ -2,14 +2,52 @@ const dboperations = require('./dboperations.js');
 const db = require('./dboperations.js');
 var World = require('./world');
 
+var fs = require('fs')
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+const { response, request } = require('express');
+const { config } = require('process');
 var app = express();
 var router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);
 
+router.use((request, response, next) => {
+    console.log('middleware');
+    next();
+})
 
-dboperations.getWorld().then(result => {
-    console.log(result);
+router.route('/world').get((request, response) => {
+    
+    dboperations.getCountrys().then(result => {
+        // console.log(result);
+        response.json(result[0]);
+    });
 });
+
+router.route('/country/:id').get((request, response) => {
+    dboperations.getCountry(request.params.id).then(result => {
+        response.json(result[0]);
+    })
+});
+
+
+router.route('/name/:name').get((request, response) => {
+    dboperations.getNameCountry(request.params.name).then(result => {
+        response.json(result[0]);
+    })
+});
+
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log('Running on Port '+ port);
+
+
+
+// dboperations.getWorld().then(result => {
+//     console.log(result);
+// });
--- dboperations.js
+++ dboperations.js
@@ -1,7 +1,7 @@
 var config = require('./config.js');
 const sql = require('mssql');
 
-async function getWorld() {
+async function getCountrys() {
     try {
         let pool = await sql.connect(config);
         let country = await pool.request().query("SELECT * FROM world");
@@ -12,6 +12,39 @@ async function getWorld() {
     }
 }
 
+async function getCountry(id) {
+    try {
+        let pool = await sql.connect(config);
+        
+        let country = await pool.request()
+            .input('input_parameter', sql.Int, id)
+            .query("SELECT * FROM world WHERE ID = @input_parameter");
+
+        return country.recordset;
+    } 
+    catch (err)
+    {
+        console.log(err);
+    }
+}
+
+async function getNameCountry(name) {
+    try {
+        let pool = await sql.connect(config);
+        
+        let country = await pool.request()
+            .input('inputName_parameter', sql.NVarChar, name)
+            .query("SELECT Name, Geom.MakeValid().STArea() as Area FROM world WHERE Name = @inputName_parameter");
+
+        return country.recordset;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
-    getWorld : getWorld
+    getCountrys : getCountrys,
+    getCountry : getCountry,
+    getNameCountry : getNameCountry
 }
